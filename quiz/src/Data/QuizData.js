785b5af51d8176f6bd0699945cb3fd6b829bfe96
Felipe Magro <66389281+FelipeMagro@users.noreply.github.com>
2025-06-04 22:13:29 -0300

    Add files via upload

---
+++ quiz/src/Data/QuizData.js
@@ -0,0 +1,62 @@
+export const QuizData = [
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 2
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    },
+    {
+        question: "Pergunta",
+        options:["Resposta1", "Resposta2", "Resposta3", "Resposta4"],
+        answer: 3
+    }
+];
